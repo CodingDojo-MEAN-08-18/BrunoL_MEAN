14ebd38e5439c92f124971303698ec8d964dc7d4
brunobohn <38817141+brunobohn@users.noreply.github.com>
2018-09-28 23:08:22 -0700

    Update server.js

--- bike/bike-marketplace/server.js
+++ bike/bike-marketplace/server.js
@@ -9,13 +9,12 @@ var path = require( "path");
 var app = express();
 
 const sessionConfig = {
-  saveUninitialized: true, // as soon as they visit the site (regardless whether they've logged in), create a cookie
-  secret: 'lksjdgkjg',
+  saveUninitialized: true, 
   resave: false,
-  name: 'session', // can be seen in the browser
+  name: 'session',
   rolling: true,
   cookie: {
-    secure: false, // should be true on a production server!!
+    secure: false, 
     httpOnly: false,
     maxAge: 360000 // expires after 100 hours? 
   }
@@ -48,5 +47,5 @@ app.use(require('./server/config/routes/catchall'))
 
 
 app.listen(80, function() {
- console.log("listening on port 80"); 
+ console.log("listening on port 8000"); 
 });
