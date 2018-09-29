8f316ae679902202a6a396afa107174b5432d1b0
brunobohn <38817141+brunobohn@users.noreply.github.com>
2018-09-28 22:58:55 -0700

    Update task.js

--- restful_tasks_crud/server/models/task.js
+++ restful_tasks_crud/server/models/task.js
@@ -1,9 +1,7 @@
-// server/models/task.js
-
 const mongoose = require('mongoose');
 
-const TaskSchema = new mongoose.Schema ({
-    title: { type: String, required: true, minlength: 3, maxlength: 64 },
+const TaskSchema = new mongoose.Schema (
+    title: { type: String, required: true, minlength: 10, maxlength: 64 },
     description: { type: String, required: true, minlength: 3, maxlength: 255 },
     completed: { type: Boolean, required: true }
 }, { timestamps: true });
