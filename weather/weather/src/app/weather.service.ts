a18fb7b8b03e54faec563dc8141c99e38ffbc851
brunobohn <38817141+brunobohn@users.noreply.github.com>
2018-09-28 23:06:52 -0700

    Update weather.service.ts

--- weather/weather/src/app/weather.service.ts
+++ weather/weather/src/app/weather.service.ts
@@ -9,24 +9,10 @@ export class WeatherService {
   weather: Object;
 
   constructor(private _http: Http) {
-  // console.log('inside weather service constructor')
-  }
-
-  /* // use a pipe instead
-  KtoC(degreesKelvin: number){
-    return degreesKelvin - 273.15
-  }
-  CtoF(degreesCelcius: number){
-    return degreesCelcius*1.8 + 32;
-  }
-  KtoF(degreesKelvin: number){
-    return this.CtoF(this.KtoC(degreesKelvin));
-  }*/
+ 
   getWeather(callback, city:City) {
-    // https://openweathermap.org/current#geo
     let apiKey: string = '54178bbaa6cdc9806737df15103cd026';
     let baseUrl: string = 'http://api.openweathermap.org/data/2.5/weather';
-    // console.log('getWeather service', city);
     this._http.get(`${baseUrl}?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`).subscribe(
       (response) => {
         this.weather = response.json();
