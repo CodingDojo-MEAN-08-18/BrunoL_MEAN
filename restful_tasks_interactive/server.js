96253abe9aab6878cfcd3964b6d6a584902fe174
brunobohn <38817141+brunobohn@users.noreply.github.com>
2018-09-28 23:00:20 -0700

    Update server.js

--- restful_tasks_interactive/server.js
+++ restful_tasks_interactive/server.js
@@ -4,14 +4,12 @@ const path = require('path');
 const app = express();
 
 app.use(bodyParser.json()); 
-
 app.use(express.static(path.join(__dirname + '/public/dist/public')));
 
 const db = 'mongodb://localhost/restful_task_api';
-
 require('./server/config/mongoose.js')(db);
 require('./server/config/routes.js')(app);
 
-app.listen(4200, function() {
-    console.log('Listening on port 4200');
+app.listen(6200, function() {
+    console.log('Listening on port 6200');
 });
